d0b10229a3f82e32e72bc5989bcb9a7856982d18
Jose Alejandro <jose.montana@adidas.com>
2023-11-18 16:17:49 -0500

    new tests for creating many cubes, it is working i still have the problem of removing the walls between them

--- Proyecto/build_Cubes.py
+++ Proyecto/build_Cubes.py
@@ -32,18 +32,67 @@ def create_cube(center, size):
         [0, 5, 4]])
 
     return vertices, faces
+# def create_multiple_cubes(n, size, spacing):
+#     all_vertices = []
+#     all_faces = []
 
-# vertices, faces = create_cube(center=[0, 0, 0], size=2)
-cube1_vertices, cube1_faces = create_cube(center=[0, 0, 0], size=2)
-cube2_vertices, cube2_faces = create_cube(center=[2, 0, 0], size=2)
+#     for i in range(n):
+#         # Calculate the center of each cube
+#         center = [i * (size + spacing), 0, 0]
 
-vertices = np.concatenate((cube1_vertices, cube2_vertices))
-faces = np.concatenate((cube1_faces, cube2_faces + len(cube1_vertices)))
+#         vertices, faces = create_cube(center, size)
+#         faces += len(all_vertices)  # Offset the face indices
+
+#         all_vertices.append(vertices)
+#         all_faces.append(faces)
+
+#     # Combine vertices and faces
+#     all_vertices = np.concatenate(all_vertices)
+#     all_faces = np.concatenate(all_faces)
+
+#     return all_vertices, all_faces
+def create_multiple_cubes(start_center, size, n, offset):
+    """
+    Create 'n' cubes, each with edge length 'size'.
+    Cubes are offset from 'start_center' by 'offset' units along the x-axis.
+    Returns the combined vertices and faces of all cubes.
+    """
+    all_vertices = []
+    all_faces = []
+
+    for i in range(n):
+        # Calculate the center for the current cube
+        center = start_center + np.array([i * offset, 0, 0])
+        # Create the cube
+        vertices, faces = create_cube(center, size)
+
+        # Append the new vertices and update faces indices
+        all_vertices.append(vertices)
+        all_faces.append(faces + (len(all_vertices)  - 1)*8)
+
+    # Combine all vertices and faces
+    combined_vertices = np.concatenate(all_vertices)
+    combined_faces = np.concatenate(all_faces)
+
+    return combined_vertices, combined_faces
+
+# Example usage
+n = 2  # Number of cubes
+vertices, faces = create_multiple_cubes(start_center=[0, 0, 0], size=2, n=n, offset=2)
+print(vertices)
+print("-"*10)
+print(faces)
+
+# n = 3  # Number of cubes
+# size = 2  # Size of each cube
+# spacing = 1  # Spacing between each cube
+# vertices, faces = create_multiple_cubes(n, size, spacing)
+# print(vertices)
+# print(faces)
 
 cube = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
 for i, f in enumerate(faces):
     for j in range(3):
-        # print(vertices[f[j],:])
         cube.vectors[i][j] = vertices[f[j]]
 
 
